8ac5090f643aad51857fedf6d467ad29f59e7221
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-24 21:18:49 +0200

    Cosmetic changes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 // steps based on the logistic function.
 //
 // https://en.wikipedia.org/wiki/Step_function
+//
 // https://en.wikipedia.org/wiki/Logistic_function
 package staircase
 
@@ -13,8 +14,8 @@ import (
 // Staircase represents a step function with smooth transitions.
 type Staircase struct {
 	lengths    []float64
-	distance   []float64
 	heights    []float64
+	distances  []float64
 	transition float64
 	steepness  float64
 }
@@ -30,16 +31,16 @@ func New(lengths, heights []float64, transition, steepness float64) (*Staircase,
 
 	n := len(lengths)
 
-	distance := make([]float64, n)
-	distance[0] = lengths[0]
+	distances := make([]float64, n)
+	distances[0] = lengths[0]
 	for i := 1; i < n; i++ {
-		distance[i] = distance[i-1] + lengths[i]
+		distances[i] = distances[i-1] + lengths[i]
 	}
 
 	staircase := &Staircase{
 		lengths:    lengths,
-		distance:   distance,
 		heights:    heights,
+		distances:  distances,
 		transition: transition,
 		steepness:  steepness,
 	}
@@ -49,8 +50,7 @@ func New(lengths, heights []float64, transition, steepness float64) (*Staircase,
 
 // Evaluate computes the function at a point.
 func (self *Staircase) Evaluate(point float64) float64 {
-	l, d, h := self.lengths, self.distance, self.heights
-	t, s := self.transition, self.steepness
+	l, d, h := self.lengths, self.distances, self.heights
 	n := len(l)
 
 	if point < 0 {
@@ -65,15 +65,15 @@ func (self *Staircase) Evaluate(point float64) float64 {
 		k++
 	}
 
+	t := self.transition
+
 	switch k {
 	case 0:
 		z := point / l[k]
 		if z >= 1-t {
 			x := (point - (1-t)*l[k]) / (t*l[k] + t*l[k+1])
 			o := l[k] / (l[k] + l[k+1])
-			return logistic(2*x-1, h[k], h[k+1], 2*o-1, s)
-		} else {
-			return h[k]
+			return logistic(2*x-1, h[k], h[k+1], 2*o-1, self.steepness)
 		}
 
 	case n - 1:
@@ -81,9 +81,7 @@ func (self *Staircase) Evaluate(point float64) float64 {
 		if z <= t {
 			x := (t*l[k-1] + point - d[k-1]) / (t*l[k-1] + t*l[k])
 			o := l[k-1] / (l[k-1] + l[k])
-			return logistic(2*x-1, h[k-1], h[k], 2*o-1, s)
-		} else {
-			return h[k]
+			return logistic(2*x-1, h[k-1], h[k], 2*o-1, self.steepness)
 		}
 
 	default:
@@ -91,15 +89,15 @@ func (self *Staircase) Evaluate(point float64) float64 {
 		if z <= t {
 			x := (t*l[k-1] + point - d[k-1]) / (t*l[k-1] + t*l[k])
 			o := l[k-1] / (l[k-1] + l[k])
-			return logistic(2*x-1, h[k-1], h[k], 2*o-1, s)
-		} else if z >= (1 - t) {
+			return logistic(2*x-1, h[k-1], h[k], 2*o-1, self.steepness)
+		} else if z >= 1-t {
 			x := (point - d[k-1] - (1-t)*l[k]) / (t*l[k] + t*l[k+1])
 			o := l[k] / (l[k] + l[k+1])
-			return logistic(2*x-1, h[k], h[k+1], 2*o-1, s)
-		} else {
-			return h[k]
+			return logistic(2*x-1, h[k], h[k+1], 2*o-1, self.steepness)
 		}
 	}
+
+	return h[k]
 }
 
 func logistic(x, a, b, offset, steepness float64) float64 {
