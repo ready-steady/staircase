29db624d866ce3f5c2bda6eece741a85951eb3ff
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-25 10:09:05 +0200

    A bit of refactoring

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 type Staircase struct {
 	lengths    []float64
 	heights    []float64
-	distances  []float64
 	transition float64
 }
 
@@ -24,18 +23,9 @@ func New(lengths, heights []float64, transition float64) (*Staircase, error) {
 		return nil, errors.New("the transition length should be in (0, 0.5]")
 	}
 
-	n := len(lengths)
-
-	distances := make([]float64, n)
-	distances[0] = lengths[0]
-	for i := 1; i < n; i++ {
-		distances[i] = distances[i-1] + lengths[i]
-	}
-
 	staircase := &Staircase{
 		lengths:    lengths,
 		heights:    heights,
-		distances:  distances,
 		transition: transition,
 	}
 
@@ -44,23 +34,23 @@ func New(lengths, heights []float64, transition float64) (*Staircase, error) {
 
 // Evaluate computes the function at a point.
 func (self *Staircase) Evaluate(point float64) float64 {
-	l, d, h := self.lengths, self.distances, self.heights
-	n := len(l)
+	h := self.heights
 
 	if point < 0 {
 		return h[0]
 	}
-	if point > d[n-1] {
-		return h[n-1]
-	}
+
+	l := self.lengths
+	n := len(l)
 
 	k := 0
-	for point > d[k] {
+	for k < n && point > l[k] {
+		point -= l[k]
 		k++
 	}
 
-	if k > 0 {
-		point -= d[k-1]
+	if k == n {
+		return h[n-1]
 	}
 
 	if z, t := point/l[k], self.transition; k > 0 && z <= t {
